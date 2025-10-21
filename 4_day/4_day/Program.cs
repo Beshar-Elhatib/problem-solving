d17159e14ec5a6e3b1fd5ac90d19378a5349efb0
Bessar Elhatib <besharelhatib@gmail.com>
2025-10-21 23:13:33 +0300

    Day 4 Challenge 21.10.2025

---
+++ 4_day/4_day/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_day
+{
+    internal class Program
+    {   /*
+         You are given an array of integers containing all the numbers from 1 to N, except for one missing number.
+         Write a C# program to find the missing number in the shortest and most efficient way possible.
+         */
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 4, 5, 6 };
+            int n = nums.Length + 1;
+            int missing = n * (n + 1) / 2 - nums.Sum();
+            Console.WriteLine($"Missing number is: {missing}");
+        }
+    }
+}
---
+++ 4_day/4_day/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("4_day")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4_day")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e862811f-ebb8-4ad8-bac8-ca876a5b4dc1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
