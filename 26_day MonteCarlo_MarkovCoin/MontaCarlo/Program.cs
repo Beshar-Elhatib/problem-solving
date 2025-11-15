8d8f7c11dc5e88cc46460ba6df1230bc02a82104
Bessar Elhatib <besharelhatib@gmail.com>
2025-11-15 23:36:49 +0300

    25 day  Initial commit: Add Markov biased coin simulation

---
+++ 26_day MonteCarlo_MarkovCoin/MontaCarlo/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+
+
+
+namespace MontaCarlo
+{
+    internal class Program
+    {
+        static string  HileliMadeniPara(double d , string OncekiDegir) 
+        {
+                if (OncekiDegir== "Tura")
+                {
+                    return d < 0.75 ? "Tura":"Yazi" ;
+                }
+                else
+                {
+                    return d < 0.5 ? "Tura" : "Yazi";
+                }
+        }
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int tekrear = 1000000; 
+            double  sayac = 0;
+
+
+            double T = rand.NextDouble();
+            string oncekiDeger = T < 0.5 ? "Tura":"Yazi";
+
+          
+            for (int i = 0; i < tekrear; i++)
+            {
+
+                double d = rand.NextDouble();
+
+                string yeni = HileliMadeniPara( d , oncekiDeger);
+
+                if (yeni == "Tura")
+                {
+                    sayac++;
+                }
+
+                oncekiDeger = yeni; 
+            }
+            Console.WriteLine("ala orani :"+ sayac/tekrear);
+        }
+    }
+}
