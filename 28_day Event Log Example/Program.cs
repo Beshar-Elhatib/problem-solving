a430ef549463fdeb03e4b20cabf9fc925d1bda14
Bessar Elhatib <besharelhatib@gmail.com>
2025-11-19 21:09:26 +0300

    28 day Event Log Example The Event Log is a logbook for the system and programmes. It helps developers record errors and important events, and is considered a powerful tool for diagnosing problems and ensuring programme quality, especially in large systems.

---
+++ 28_day Event Log Example/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+
+        //The Event Log is a logbook for the system and programmes.
+        //It helps developers record errors and important events,
+        //and is considered a powerful tool for diagnosing problems
+        //and ensuring programme quality, especially in large systems.
+
+        // Specify the source name for the event log
+        string sourceName = "MyApp";
+
+
+        // Create the event source if it does not exist
+        if (!EventLog.SourceExists(sourceName))
+        {
+            EventLog.CreateEventSource(sourceName, "Application");
+            Console.WriteLine("Event source created.");
+        }
+
+
+        // Log an information event
+        EventLog.WriteEntry(sourceName, "This is My information :-).", EventLogEntryType.Information);
+        
+        // Log a Warning event
+        EventLog.WriteEntry(sourceName, "This My Warning :-)", EventLogEntryType.Warning);
+
+        // Log an Error event
+        EventLog.WriteEntry(sourceName, "This My Error :-)", EventLogEntryType.Error);
+
+
+        Console.WriteLine("Event written to the log.");
+        Console.ReadKey();
+
+    }
+}
