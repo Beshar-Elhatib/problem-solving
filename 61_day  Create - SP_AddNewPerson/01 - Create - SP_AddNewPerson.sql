191c53edc14834564b9de4490c808c61789e7169
Bessar Elhatib <besharelhatib@gmail.com>
2026-01-01 22:44:28 +0300

    61 day Create - SP_AddNewPerson

---
+++ 61_day  Create - SP_AddNewPerson/01 - Create - SP_AddNewPerson.sql
@@ -0,0 +1,29 @@
+CREATE PROCEDURE SP_AddNewPerson
+    @FirstName NVARCHAR(100),
+    @LastName NVARCHAR(100),
+    @Email NVARCHAR(255),
+    @NewPersonID INT OUTPUT
+AS
+BEGIN
+    INSERT INTO People (FirstName, LastName, Email)
+    VALUES (@FirstName, @LastName, @Email);
+
+
+    SET @NewPersonID = SCOPE_IDENTITY();
+END
+
+
+use C21_DB1; 
+DECLARE @PersonID INT;
+EXEC SP_AddNewPerson 
+    @FirstName = 'John', 
+    @LastName = 'Doe', 
+    @Email = 'john.doe@example.com',
+    @NewPersonID = @PersonID OUTPUT;
+
+SELECT @PersonID AS NewPersonID;
+
+
+select * from people
+
+
